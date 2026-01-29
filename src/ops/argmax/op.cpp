1a439b81245975c618c1a33b8262a9788ad0e59f
ArcaLunar <arca@connect.hku.hk>
2026-01-29 18:50:20 +0800

    2.1 argmax

--- src/ops/argmax/op.cpp
+++ src/ops/argmax/op.cpp
@@ -1,7 +1,18 @@
 #include "op.hpp"
+#include "../../utils.hpp"
+#include "cpu/argmax_cpu.hpp"
+#include "llaisys.h"
 
 namespace llaisys::ops {
 void argmax(tensor_t max_idx, tensor_t max_val, tensor_t vals) {
-    TO_BE_IMPLEMENTED();
+    ASSERT(max_idx->numel() == 1, "argmax(): max_idx must have exactly one element");
+    ASSERT(max_val->numel() == 1, "argmax(): max_val must have exactly one element");
+    ASSERT(vals->isContiguous(), "argmax(): input tensor must be contiguous");
+
+    llaisysDeviceType_t device = vals->deviceType();
+    if (device == LLAISYS_DEVICE_CPU)
+        cpu::argmax(max_idx->data(), max_val->data(), vals->data(), vals->numel(), vals->dtype());
+    else
+        EXCEPTION_UNSUPPORTED_DEVICE;
 }
 } // namespace llaisys::ops
--- src/utils.hpp
+++ src/utils.hpp
@@ -1,3 +1,5 @@
 #pragma once
 #include "utils/check.hpp"
 #include "utils/types.hpp"
+
+#define casting(T, v) llaisys::utils::cast<T>(v)
